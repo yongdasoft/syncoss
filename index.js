7b75ebb6894b7ffe78b9b9d931d647da3cb1471b
龙仕云 <mrlong.com@gmail.com>
2017-08-21 15:35:37 +0800

    修改：  1.修正只有变化的才上传。提交效率 作者：龙仕云

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+///////////////////////////////////////////////////
+//
+//  同步数据到osss
+// 作者：龙仕云  2017-8-21
+//
+//
+// 编号   作者     日期       内容:
+//  1   龙仕云   2017-8-21  只有变化的才处理
+//
+////////////////////////////////////////////////////
+
 var OSS = require('ali-oss');
 var  Config = require('./config.js');
 var co = require('co');
 var fs = require('fs');
+var crypto = require('crypto');
+var async = require('async');
 
 
 var client = new OSS({
@@ -13,6 +26,7 @@ var client = new OSS({
 
 
 co(function* () {
+  
   client.useBucket(Config.bucket);
   var result = yield client.list({
     prefix:Config.subdir
@@ -20,9 +34,9 @@ co(function* () {
   
 
   //删除不要的文件
-  for(var i=0,ii=result.objects ? result.objects.length:0;i<ii;i++){
-    yield client.delete(result.objects[i].name);  
-  };
+  //for(var i=0,ii=result.objects ? result.objects.length:0;i<ii;i++){
+  //  yield client.delete(result.objects[i].name);  
+  //};
   
   //1.取出版本来
   var verfile = __dirname + '/ver.json';
@@ -30,35 +44,109 @@ co(function* () {
     var str = fs.readFileSync(verfile,"utf8");
     var verjson=JSON.parse(str);
         
-    for(var i=0,ii=verjson.filedata.length;i<ii;i++){
-      var params = verjson.filedata[i];
-      var params = params.split(',');
+    //for(var i=0,ii=verjson.filedata.length;i<ii;i++){
+    var filedata=[];
+    async.eachSeries(verjson.filedata,function(curfile,cb){  
+      
+      var params = curfile.split(',');
+      
       if(params.length==4){
-        var filename = __dirname + '/filedata/' + params[2];  
-        console.log(filename);
-        var result = yield client.put(Config.subdir + '/filedata/' + params[2],filename);
-        if(result.res.status==200){
-          params[3] = result.res.headers.etag;   
-          verjson.filedata[i] = params.join(',');
-        }
-        else{
-           console.error("上传文件出错。" + filename);       
-        }
+        
+        var filename    = __dirname + '/filedata/' + params[2];  //本地的
+        var ossfilename = Config.subdir + '/filedata/' + params[2]; //oss的
+        var ossmd5 ;
+        co(function*(){
+          try{
+            var result = yield client.head(ossfilename);
+            ossmd5 = result.res.headers.etag.replace(/"/g,'');
+
+            if(ossmd5){
+              var md5sum = crypto.createHash('md5');  
+              var stream = fs.createReadStream(filename);
+              stream.on('data', function(chunk){
+                md5sum.update(chunk);
+              });
+              stream.on('end', function() {
+                str = md5sum.digest('hex').toUpperCase();
+                if (str == ossmd5.toUpperCase()){
+                  console.log('不更新='+filename);
+                  params[3] = str;
+                  curfile = params.join(',');
+                  filedata.push(curfile);
+                  cb(null);
+                }
+                else{
+                  console.log('更新='+filename);
+                  co(function*() {
+                    var result = yield client.put(ossfilename,filename);
+                    if(result.res.statusCode==200){
+                      params[3] = result.res.headers.etag;   
+                      curfile = params.join(',');
+                      filedata.push(curfile);
+                      cb(null);
+                    }
+                    else{
+                      cb(new Error("上传文件出错。" + filename))
+                    };
+                  });//end co
+                  
+                }; //end md5
+              }); //end stream
+
+            } else {
+              console.log('新增='+filename);
+              var result = yield client.put(ossfilename,filename);
+              if(result.res.status==200){
+                params[3] = result.res.headers.etag;   
+                curfile = params.join(',');
+                filedata.push(curfile);
+                
+              }
+              else{
+               console.error("上传文件出错。" + filename);       
+              };
+            };
+
+          } catch(err){
+
+            console.log('新增='+filename);
+            var result = yield client.put(ossfilename,filename);
+            if(result.res.status==200){
+              params[3] = result.res.headers.etag;   
+              curfile = params.join(',');
+              filedata.push(curfile);
+            }
+            else{
+             console.error("上传文件出错。" + filename);       
+            };
+
+          };
+          
+        }); //end co
+        
+      }else{
+        cb(new Error("参数出错。"));
+      }
+      
+    },function(err){
+      
+      if(!err){
+        co(function*(){
+        //3.回写内容到ver.json 因为有md5码的更新,并上传
+        var newfile = __dirname + '/ver1.json';
+        verjson.filedata = filedata;
+        fs.writeFileSync(newfile, JSON.stringify(verjson));
+        yield client.put(Config.subdir + '/ver.json' ,newfile);
+        });
       }
       else{
-        console.error("参数出错。");   
-      }   
-    };
-    
-    //3.回写内容到ver.json 因为有md5码的更新,并上传
-    var newfile = __dirname + '/ver1.json';
-    fs.writeFileSync(newfile, JSON.stringify(verjson));
-    yield client.put(Config.subdir + '/ver.json' ,newfile);
+        console.error(err.toString());
+      }
+    });
   }
   else{
     console.log('没有找到ver.json文件');    
   }
-  
   
 }).catch(function (err) {
   console.log(err);
